e7f8bed703498fa3a3a1b92be4aa651bf8e2b96a
Darren <ninenumbproductions@gmail.com>
2018-05-02 11:35:05 -0700

    try again

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 exports.PORT = process.env.PORT || 8080;
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://thinkful-darren:dev@localhost/noteful';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
 
